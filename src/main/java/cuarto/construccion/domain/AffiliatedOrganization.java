216bcf070b0ee8769db8821a2a1b2b0493cc1a28
samcb03 <cbsam@gmail.com>
2026-03-25 01:18:44 -0600

    Creates the first seven classes in the domain package: User, Student, ProjectSupervisor, Project, Professor, AffiliatedOrganization each one with their attributes, constructors, getters and setters

--- src/main/java/cuarto/construccion/domain/AffiliatedOrganization.java
+++ src/main/java/cuarto/construccion/domain/AffiliatedOrganization.java
@@ -1,5 +1,89 @@
 package cuarto.construccion.domain;
 
-public class AffiliatedOrganization {
 
+public class AffiliatedOrganization {
+    private int idAffiliatedOrganization;
+    private String nameAffiliatedOrganization;
+    private String city;
+    private String state;
+    private String email;
+    private String phoneNumber;
+    private int numberOfDirectUsers;
+    private int numberOfIndirectUsers;
+   
+    public AffiliatedOrganization(int idAffiliatedOrganization, String nameAffiliatedOrganization, String city,
+            String state, String email, String phoneNumber, int numberOfDirectUsers, int numberOfIndirectUsers) {
+        this.idAffiliatedOrganization = idAffiliatedOrganization;
+        this.nameAffiliatedOrganization = nameAffiliatedOrganization;
+        this.city = city;
+        this.state = state;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.numberOfDirectUsers = numberOfDirectUsers;
+        this.numberOfIndirectUsers = numberOfIndirectUsers;
+    }
+
+    public int getIdAffiliatedOrganization() {
+        return idAffiliatedOrganization;
+    }
+
+    public void setIdAffiliatedOrganization(int idAffiliatedOrganization) {
+        this.idAffiliatedOrganization = idAffiliatedOrganization;
+    }
+
+    public String getNameAffiliatedOrganization() {
+        return nameAffiliatedOrganization;
+    }
+
+    public void setNameAffiliatedOrganization(String nameAffiliatedOrganization) {
+        this.nameAffiliatedOrganization = nameAffiliatedOrganization;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getNumberOfDirectUsers() {
+        return numberOfDirectUsers;
+    }
+
+    public void setNumberOfDirectUsers(int numberOfDirectUsers) {
+        this.numberOfDirectUsers = numberOfDirectUsers;
+    }
+
+    public int getNumberOfIndirectUsers() {
+        return numberOfIndirectUsers;
+    }
+
+    public void setNumberOfIndirectUsers(int numberOfIndirectUsers) {
+        this.numberOfIndirectUsers = numberOfIndirectUsers;
+    } 
 }
--- src/main/java/cuarto/construccion/domain/Project.java
+++ src/main/java/cuarto/construccion/domain/Project.java
@@ -1,5 +1,69 @@
 package cuarto.construccion.domain;
 
-public class Project {
 
+public class Project {
+    private int idProject;
+    private String projectName;
+    private String projectMethodology;
+    private int capacity;
+    private String projectObjective;
+    private String projectDescription;
+    
+    public Project(int idProject, String projectName, String projectMethodology, int capacity, 
+            String projectObjective, String projectDescription) {
+        this.idProject = idProject;
+        this.projectName = projectName;
+        this.projectMethodology = projectMethodology;
+        this.capacity = capacity;
+        this.projectObjective = projectObjective;
+        this.projectDescription = projectDescription;
+    }
+
+    public int getIdProject() {
+        return idProject;
+    }
+
+    public void setIdProject(int idProject) {
+        this.idProject = idProject;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectMethodology() {
+        return projectMethodology;
+    }
+
+    public void setProjectMethodology(String projectMethodology) {
+        this.projectMethodology = projectMethodology;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getProjectObjective() {
+        return projectObjective;
+    }
+
+    public void setProjectObjective(String projectObjective) {
+        this.projectObjective = projectObjective;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+
+    public void setProjectDescription(String projectDescription) {
+        this.projectDescription = projectDescription;
+    }  
 }
