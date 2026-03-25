5fba65f1e31c8c4005030c528697d5d4958ab57e
samcb03 <cbsam@gmail.com>
2026-03-24 21:53:37 -0600

    PruebaCommit

---
+++ src/main/java/cuarto/construccion/Main.java
@@ -0,0 +1,7 @@
+package cuarto.construccion;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/cuarto/construccion/dataaccess/MySQLConnectionManager.java
@@ -0,0 +1,23 @@
+package cuarto.construccion.dataaccess;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class MySQLConnectionManager {
+    
+    private static String databaseUrl = "jdbc:mysql://localhost:3306/PracticasSoftware"
+         + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC-6";
+    private static String user = "Administrador";
+    private static String password = "admin_DB@135!";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(databaseUrl, user, password);
+    } 
+
+    public static MySQLConnectionManager getInstance() {
+        return new MySQLConnectionManager();
+    }
+}
