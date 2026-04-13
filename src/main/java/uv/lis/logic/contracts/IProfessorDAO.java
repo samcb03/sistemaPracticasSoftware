2e08aa53ec485798bb49e6b1f0904b7e4ebe6dd9
samcb03 <cbsam@gmail.com>
2026-04-13 14:04:57 -0600

    Herencia de UserDAO a StudentDAO y ProfessorDAO

---
+++ src/main/java/uv/lis/logic/contracts/IProfessorDAO.java
@@ -0,0 +1,16 @@
+package uv.lis.logic.contracts;
+
+
+import java.sql.SQLException;
+import uv.lis.logic.dto.Professor;
+
+
+public interface IProfessorDAO {
+    Professor getProfessorByPersonalNumber(String idProfessor) throws SQLException;
+
+    boolean registerProfessor(Professor professor) throws SQLException;
+
+    boolean modifyProfessor(Professor professor) throws SQLException;
+
+    boolean inactivateProfessor(Professor professor) throws SQLException;
+}
---
+++ src/main/java/uv/lis/logic/dao/StudentDAO.java
@@ -0,0 +1,138 @@
+package uv.lis.logic.dao;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import uv.lis.dataaccess.MySQLConnectionManager;
+import uv.lis.logic.contracts.IStudentDAO;
+import uv.lis.logic.dto.Student;
+
+
+public class StudentDAO extends UserDAO implements IStudentDAO {
+    private static final int NO_ROWS_AFFECTED = 0;
+    private static final Logger LOGGER = Logger.getLogger(UserDAO.class.getName());
+    private MySQLConnectionManager connectionManager;
+
+    @Override
+    public Student getStudentById(String idStudent) { 
+        Student student = null;
+
+    String studentQuery = "SELECT e.idUsuario, e.matricula, u.nombre, u.apellidos, e.fechaNacimiento, e.genero," 
+        + "e.lenguaIndigena FROM Alumno e INNER JOIN Usuario u ON e.idUsuario = u.idUsuario WHERE e.matricula = ?;"; 
+
+        try (Connection databaseConnection = connectionManager.getConnection();
+             PreparedStatement preparedStatement = databaseConnection.prepareStatement(studentQuery)) {
+            
+            preparedStatement.setString(1, idStudent);
+            
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    student = new Student();
+                    student.setId(resultSet.getInt("idUsuario"));
+                    student.setIdStudent(resultSet.getString("matricula"));
+                    student.setFirstName(resultSet.getString("nombre"));
+                    student.setLastName(resultSet.getString("apellidos"));
+                    student.setBirthDate(resultSet.getDate("fechaNacimiento"));
+                    student.setGender(resultSet.getString("genero"));
+                    student.setIndigenousLanguage(resultSet.getBoolean("lenguaIndigena"));
+
+                    LOGGER.log(Level.INFO, "Busqueda de responsable de proyecto con matricula {0} exitosa.", 
+                        idStudent);
+                } else {
+                    LOGGER.log(Level.INFO, "No se encontro un alumno con la matricula {0}.", idStudent);
+                }
+            }
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Error de conexion con la base de datos",e);
+        }
+        
+        return student;
+    }
+
+    @Override
+    public boolean registerStudent(Student student) {
+        boolean isRegistered = false;
+
+        String studentQuery = "INSERT INTO Alumno (idUsuario, matricula, fechaNacimiento, genero, lenguaIndigena," 
+            + "estado) VALUES (?, ?, ?, ?, ?, ?);";
+
+        try (Connection databaseConnection = connectionManager.getConnection();
+             PreparedStatement preparedStatement = databaseConnection.prepareStatement(studentQuery)) {
+            
+            preparedStatement.setInt(1, student.getId());
+            preparedStatement.setString(2, student.getIdStudent());
+            java.util.Date birthDate = student.getBirthDate();
+            java.sql.Date sqlBirthDate = new java.sql.Date(birthDate.getTime());
+
+            preparedStatement.setDate(3, sqlBirthDate);
+            preparedStatement.setString(4, student.getGender());
+            preparedStatement.setBoolean(5, student.hasIndigenousLanguage());
+            preparedStatement.setBoolean(6, student.isInactive());
+
+            if (preparedStatement.executeUpdate() > NO_ROWS_AFFECTED) {
+                isRegistered = true;
+                LOGGER.log(Level.INFO, "Registro de alumno con matricula {0} exitoso.", student.getIdStudent());
+            }
+
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Error de conexion con la base de datos",e);
+        }
+        
+        return isRegistered;
+    }
+
+    @Override
+    public boolean modifyStudent(Student student) {
+        boolean isModified = false;
+
+        String studentQuery = "UPDATE Alumno e " 
+            + "INNER JOIN Usuario u ON e.idUsuario = u.idUsuario SET e.matricula = ?, u.nombre = ?, u.apellidos = ? " 
+            + "WHERE e.idUsuario = ?;";
+
+        try (Connection databaseConnection = connectionManager.getConnection();
+             PreparedStatement preparedStatement = databaseConnection.prepareStatement(studentQuery)) {
+            
+            preparedStatement.setString(1, student.getIdStudent());
+            preparedStatement.setString(2, student.getFirstName());
+            preparedStatement.setString(3, student.getLastName());
+            preparedStatement.setInt(4, student.getId());
+
+            if (preparedStatement.executeUpdate() > NO_ROWS_AFFECTED) {
+                isModified = true;
+                LOGGER.log(Level.INFO, "Modificacion de alumno con matricula {0} exitosa.", student.getIdStudent());
+            }
+
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Error de conexion con la base de datos",e);
+        }
+
+        return isModified;
+    }
+
+    @Override
+    public boolean inactivateStudent(Student student) {
+        boolean isInactive = false;
+        
+        String studentQuery = "UPDATE Alumno SET estado = 0 WHERE idUsuario = ?;";
+
+        try (Connection databaseConnection = connectionManager.getConnection();
+             PreparedStatement preparedStatement = databaseConnection.prepareStatement(studentQuery)) {
+            
+            preparedStatement.setInt(1, student.getId());
+
+            if (preparedStatement.executeUpdate() > NO_ROWS_AFFECTED) {
+                isInactive = true;
+                LOGGER.log(Level.INFO, "Inactivacion de alumno con matricula exitosa.", student.getIdStudent());
+            }
+
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Error de conexion con la base de datos",e);
+        }
+
+        return isInactive;
+    }
+}
