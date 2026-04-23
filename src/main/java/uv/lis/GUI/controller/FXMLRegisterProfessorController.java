d1487656c197f5575582ac9d1a28b61590eab46f
dennys1020 <denisseyamileth57@gmail.com>
2026-04-23 12:16:53 -0600

    Modificacion del controller registerProfesor, validando cada campo individualmente y agregando variables estáticas utilizando regex en variables como la contraseña, numero de personal y nombres

--- src/main/java/uv/lis/GUI/controller/FXMLRegisterProfessorController.java
+++ src/main/java/uv/lis/GUI/controller/FXMLRegisterProfessorController.java
@@ -3,11 +3,13 @@ package uv.lis.GUI.controller;
 
 import uv.lis.logic.dto.Professor;
 import uv.lis.logic.exceptions.OperationException;
-import uv.lis.InputValidator;
 import uv.lis.logic.dao.ProfessorDAO;
 import uv.lis.logic.dao.UserDAO;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.stream.Stream;
+
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,16 +19,21 @@ import javafx.stage.Stage;
 
 public class FXMLRegisterProfessorController implements Initializable {
 
+    private static final int MAX_FIRST_NAME_LENGTH = 50;
+    private static final int MAX_LAST_NAME_LENGHT = 150;
+    private static final String PASSWORD_REGEX = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$";
+    private static final String LETTERS_ONLY_REGEX = "[\\p{L}\\s]+";
+    private static final String ONLY_NUMBERS = "\\d+";
+
     @FXML private Button buttonBack;
     @FXML private Button buttonRegister;
-
     @FXML private TextField txtFirstName;
     @FXML private TextField txtLastName;
     @FXML private PasswordField txtPassword;
     @FXML private TextField txtPersonnelNumber;
-
     @FXML private ComboBox<String> comboBoxCoordinator;
-    @FXML private Label labelError;
+    
+    @FXML private Label labelMessage;
 
     private UserDAO userDAO;
     private ProfessorDAO professorDAO;
@@ -43,41 +50,69 @@ public class FXMLRegisterProfessorController implements Initializable {
 
     @FXML
     public void validateFields() {
-        String error;
-
-        error = InputValidator.validateName(txtFirstName, "El nombre", 50);
-        if (error != null) {
-            showError(error);
-            return;
+        Optional<String> message = getFirstValidationMessage();
+        if (message.isPresent()) {
+            showMessage(message.get());
+        } else {
+            labelMessage.setText("");
+            registerProfessor();
         }
-
-        error = InputValidator.validateName(txtLastName, "Los apellidos", 50);
-        if (error != null) {
-            showError(error);
-            return;
-        }
-
-        error = InputValidator.validatePassword(txtPassword);
-        if (error != null) {
-            showError(error);
-            return;
-        }
-
-        error = InputValidator.validateRequired(txtPersonnelNumber, "El número de personal");
-        if (error != null) {
-            showError(error);
-            return;
-        }
-
-        if (comboBoxCoordinator.getValue() == null) {
-            showError("Seleccione si es coordinador o no");
-            return;
-        }
-
-        labelError.setText("");
-        registerProfessor();
     }
 
+   private Optional<String> getFirstValidationMessage() {
+        return Stream.of(
+            validateFirstName(txtFirstName.getText().trim(), "El nombre"),
+            validateLastName(txtLastName.getText().trim(), "El apellido"),
+            validatePassword(txtPassword.getText().trim(),"La contraseña"),
+            validatePersonnelNumber(txtPersonnelNumber.getText().trim(), "El numero de personal")
+        )
+        .filter(Optional::isPresent)
+        .map(Optional::get)
+        .findFirst();
+    }
+
+    private Optional<String> validateFirstName(String firstName, String field) {
+        Optional<String> message = Optional.empty();
+        if (firstName.isEmpty() || firstName.length() > MAX_FIRST_NAME_LENGTH) {
+            message = Optional.of("El nombre no puede estar vacío o tener más de "
+                + MAX_FIRST_NAME_LENGTH + " caracteres");
+        } else if(!firstName.matches(LETTERS_ONLY_REGEX)){
+            message = Optional.of("El nombre solo pueden contener letras");
+        }
+        return message;
+    }
+
+    private Optional<String> validateLastName(String lastName, String field) {
+        Optional<String> message;
+        if(lastName.isEmpty() || lastName.length() > MAX_LAST_NAME_LENGHT) {
+            message = Optional.of("El apellido no puede estar vacío o tener más de "
+                + MAX_LAST_NAME_LENGHT + " caracteres");
+        } else {
+            message = Optional.empty();
+        }
+        return message;
+    }
+
+    private Optional<String> validatePassword(String password, String field) {
+        Optional<String> message = Optional.empty();
+        if (password.isEmpty()) {
+            message = Optional.of("La contraseña no puede estar vacía");
+        } else if(!password.matches(PASSWORD_REGEX)) {
+            message = Optional.of("La contraseña no tiene un formato válido");
+        } 
+        return message;
+    }
+
+    private Optional<String> validatePersonnelNumber(String personnelNumber, String field) {
+        Optional<String> message = Optional.empty();
+        if (personnelNumber.isEmpty()) {
+            message = Optional.of("El numero de personal no puede estar vacío");
+        } else if (!personnelNumber.matches(ONLY_NUMBERS)){
+            message = Optional.of("El numero de personal no tiene un formato válido");
+        }
+        return message;
+    }
+    
     private void registerProfessor() {
 
         Professor professor = buildProfessor();
@@ -94,14 +129,14 @@ public class FXMLRegisterProfessorController implements Initializable {
                     showSuccess("Profesor registrado correctamente");
                     clearFields();
                 } else {
-                    showError("Error al registrar profesor");
+                    showMessage("message al registrar profesor");
                 }
             } else {
-                showError("Error registrando usuario");
+                showMessage("message registrando usuario");
             }
 
         } catch (OperationException e) {
-            showError(e.getMessage());
+            showMessage(e.getMessage());
         }
     }
 
@@ -137,14 +172,14 @@ public class FXMLRegisterProfessorController implements Initializable {
         comboBoxCoordinator.setValue(null);
     }
 
-    private void showError(String message) {
-        labelError.setText(message);
-        labelError.setStyle("-fx-text-fill: red;");
+    private void showMessage(String message) {
+        labelMessage.setText(message);
+        labelMessage.setStyle("-fx-text-fill: red;");
     }
 
     private void showSuccess(String message) {
-        labelError.setText(message);
-        labelError.setStyle("-fx-text-fill: green;");
+        labelMessage.setText(message);
+        labelMessage.setStyle("-fx-text-fill: green;");
 
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Éxito");
